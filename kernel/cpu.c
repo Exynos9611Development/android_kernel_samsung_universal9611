dc783671beb68822f1b2d4a9fc30a1b8153a7461
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-09 18:01:49 -0800

    kernel: Add bi-cluster API to affine IRQs and kthreads to fast CPUs
    On devices with a CPU that contains heterogeneous cores (e.g.,
    big.LITTLE), it can be beneficial to place some performance-critical
    IRQs and kthreads onto the fast CPU clusters in order to improve
    performance.
    
    This commit adds the following APIs:
    -kthread_run_perf_critical() to create and start a perf-critical kthread
     and affine it to the given performance CPUs
    -irq_set_perf_affinity() to mark an active IRQ as perf-critical and
     affine it to the given performance CPUs
    -IRQF_PERF_AFFINE to schedule an IRQ and any
     threads it may have onto the given performance CPUs
    -PF_PERF_CRITICAL to mark a process (mainly a kthread) as performance
     critical (this is used by kthread_run_perf_critical())
    
    In order to accommodate this new API, the following changes are made:
    -Performance-critical IRQs are distributed evenly among online CPUs
     available in the given performance CPU mask
    -Performance-critical IRQs have their affinities reaffined upon exit
     from suspend (since the affinities are broken when non-boot CPUs are
     disabled)
    -Performance-critical IRQs and their threads have their affinities reset
     upon entering suspend, so that upon immediate suspend exit (when only
     the boot CPU is online), interrupts can be processed and interrupt
     threads can be scheduled onto an online CPU (otherwise we'd hit a
     kernel BUG)
    -__set_cpus_allowed_ptr() is modified to enforce a performance-critical
     kthread's affinity
    -Perf-critical IRQs aren't allowed to have their affinity changed by
     userspace
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    (cherry picked from commit 6dd57c8cb3d82ec6965073ef2c8e49d516d17245)
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    [TenSeventy7: Port for Exynos9610 platform]
    Signed-off-by: John Vincent <git@tensevntysevn.cf>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -30,6 +30,7 @@
 #include <linux/relay.h>
 #include <linux/slab.h>
 #include <linux/percpu-rwsem.h>
+#include <linux/interrupt.h>
 #include <linux/sec_debug.h>
 #include <linux/cpuset.h>
 #include <linux/random.h>
@@ -1235,7 +1236,17 @@ static int cpu_down_maps_locked(unsigned int cpu, enum cpuhp_state target)
 
 static int do_cpu_down(unsigned int cpu, enum cpuhp_state target)
 {
+	struct cpumask newmask;
 	int err;
+
+	preempt_disable();
+	cpumask_andnot(&newmask, cpu_online_mask, cpumask_of(cpu));
+	preempt_enable();
+
+	/* One big, and LITTLE CPU must remain online */
+	if (!cpumask_intersects(&newmask, cpu_lp_mask) ||
+	    !cpumask_intersects(&newmask, cpu_perf_mask))
+		return -EINVAL;
 
 	cpu_maps_update_begin();
 	err = cpu_down_maps_locked(cpu, target);
@@ -1441,6 +1452,7 @@ int freeze_secondary_cpus(int primary)
 	int cpu, error = 0;
 
 	cpu_maps_update_begin();
+	unaffine_perf_irqs();
 	if (!cpu_online(primary))
 		primary = cpumask_first(cpu_online_mask);
 	/*
@@ -1527,6 +1539,7 @@ void enable_nonboot_cpus(void)
 	arch_enable_nonboot_cpus_end();
 
 	cpumask_clear(frozen_cpus);
+	reaffine_perf_irqs(false);
 out:
 	cpu_maps_update_done();
 }
