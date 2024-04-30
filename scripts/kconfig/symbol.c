55f5492008885282907cc991a085d3f77f486c42
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-04-30 13:53:31 +0530

    scripts: fix misleading indentation warning

--- scripts/kconfig/symbol.c
+++ scripts/kconfig/symbol.c
@@ -1150,7 +1150,6 @@ static void sym_check_print_recursive(struct symbol *last_sym)
 		if (stack->sym == last_sym)
 			fprintf(stderr, "%s:%d:error: recursive dependency detected!\n",
 				prop->file->name, prop->lineno);
-
 		if (stack->expr) {
 			fprintf(stderr, "%s:%d:\tsymbol %s %s value contains %s\n",
 				prop->file->name, prop->lineno,
