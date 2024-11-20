8718d02cb1fa3e82e6877e051b3c0e0c9b3b07bf
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-21 01:59:51 +0800

    /proc/pid/maps: dont allow paths with lineage to be listed

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -530,10 +530,16 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	const char *name = NULL;
 
 	if (file) {
-		struct inode *inode = file_inode(vma->vm_file);
-		dev = inode->i_sb->s_dev;
-		ino = inode->i_ino;
-		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+    	struct inode *inode = file_inode(vma->vm_file);
+    	struct dentry *dentry;
+    	dev = inode->i_sb->s_dev;
+    	ino = inode->i_ino;
+    	pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+    	dentry = file->f_path.dentry;
+    	if (dentry) {
+        	const char *path = (const char *)dentry->d_name.name; 
+            	if (strstr(path, "lineage")) { return; }
+            	}
 	}
 
 	start = vma->vm_start;
