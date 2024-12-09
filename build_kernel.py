e67b2454b0185e8f418d6f5cb11360d9001e46a3
Cat658011 <cat658011@gmail.com>
2024-12-09 19:26:43 +0500

    SN: Bump to 1.2.0 - Horror

--- build_kernel.py
+++ build_kernel.py
@@ -74,8 +74,8 @@ def main():
         'ARCH=arm64', f'-j{os.cpu_count()}'
     ]
  
-    kernel_version = "1.1.0"
-    kernel_additional_name = "Dust"
+    kernel_version = "1.2.0"
+    kernel_additional_name = "Horror"
 
     if not file_exists("AnyKernel3/anykernel.sh"):
         run_command(['git', 'submodule', 'update', '--init'])
