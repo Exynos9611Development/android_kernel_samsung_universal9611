180e9290d99a88a6c5af7a1aa332882996deb628
Cat658011 <cat658011@gmail.com>
2025-08-27 17:29:44 +0500

    kernel: power: Fix build without CONFIG_SEC_DEBUG

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -22,7 +22,10 @@
 #include <linux/kmod.h>
 #include <trace/events/power.h>
 #include <linux/cpuset.h>
+
+#ifdef CONFIG_SEC_DEBUG
 #include <linux/sec_debug.h>
+#endif
 #include <soc/samsung/exynos-debug.h>
 
 /*
@@ -42,6 +45,7 @@ static int try_to_freeze_tasks(bool user_only)
 	bool wakeup = false;
 	int sleep_usecs = USEC_PER_MSEC;
 
+#ifdef CONFIG_SEC_DEBUG
 	char *sys_state[SYSTEM_END] = {
 		"BOOTING",
 		"SCHEDULING",
@@ -50,6 +54,7 @@ static int try_to_freeze_tasks(bool user_only)
 		"POWER_OFF",
 		"RESTART",
 	};
+#endif
 
 	start = ktime_get_boottime();
 
@@ -60,8 +65,10 @@ static int try_to_freeze_tasks(bool user_only)
 	if (!user_only)
 		freeze_workqueues_begin();
 
+#ifdef CONFIG_SEC_DEBUG
 	sec_debug_set_unfrozen_task((uint64_t)NULL);
 	sec_debug_set_unfrozen_task_count((uint64_t)0);
+#endif
 
 	while (true) {
 		todo = 0;
@@ -72,10 +79,14 @@ static int try_to_freeze_tasks(bool user_only)
 
 			if (!freezer_should_skip(p)) {
 				todo++;
+#ifdef CONFIG_SEC_DEBUG
 				sec_debug_set_unfrozen_task((uint64_t)p);
+#endif
 			}
 		}
+#ifdef CONFIG_SEC_DEBUG
 		sec_debug_set_unfrozen_task_count((uint64_t)todo);
+#endif
 
 		read_unlock(&tasklist_lock);
 
@@ -125,13 +136,17 @@ static int try_to_freeze_tasks(bool user_only)
 			if (p != current && !freezer_should_skip(p)
 			    && freezing(p) && !frozen(p)) {
 				sched_show_task(p);
+#ifdef CONFIG_SEC_DEBUG
 				sec_debug_set_extra_info_backtrace_task(p);
 				sec_debug_set_extra_info_unfz(p->comm);
+#endif
 			}
 		}
 		read_unlock(&tasklist_lock);
 
+#ifdef CONFIG_SEC_DEBUG
 		sec_debug_set_extra_info_unfz(sys_state[system_state]);
+#endif
 #if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
 		panic("fail to freeze tasks");
 #endif
@@ -142,8 +157,10 @@ static int try_to_freeze_tasks(bool user_only)
 
 	s3c2410wdt_emergency_multistage_wdt_start();
 
+#ifdef CONFIG_SEC_DEBUG
 	sec_debug_set_unfrozen_task((uint64_t)NULL);
 	sec_debug_set_unfrozen_task_count((uint64_t)0);
+#endif
 
 	return todo || wakeup ? -EBUSY : 0;
 }
